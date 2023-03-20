28eddc98f4f3e2cc4427dfdde43f9e57cb5e5ede
nham ngo <nhamngoinfo@gmail.com>
2023-03-20 21:24:57 +0700

    action bar and menu

--- app/src/main/java/com/example/OnThiBangLaiXe/MainActivity.java
+++ app/src/main/java/com/example/OnThiBangLaiXe/MainActivity.java
@@ -1,14 +1,49 @@
 package com.example.OnThiBangLaiXe;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.cardview.widget.CardView;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
 
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    NavigationView nav_view;
+    DrawerLayout drawerLayout;
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawerlayout);
+        nav_view=(NavigationView) findViewById(R.id.nav_Main);
+        toolbar =(Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        nav_view.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        nav_view.setCheckedItem(R.id.item_Home);
+        Menu menu = nav_view.getMenu();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Menu menu = nav_view.getMenu();
+        Intent intent;
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return false;
     }
 }
